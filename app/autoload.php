65ac04b74119bf34ea103e284e3516dc94df1374
sushil rangari <sushil.rangari@perficient.com>
2021-08-01 21:37:29 +0530

    added

--- app/autoload.php
+++ app/autoload.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 use Magento\Framework\Autoload\AutoloaderRegistry;
 use Magento\Framework\Autoload\ClassLoaderWrapper;
 
+
 /**
  * Shortcut constant for the root directory
  */
